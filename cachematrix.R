d531bda8acd7e2754b03bc4f83a06df100c08493
bharathilaxman <bharathil@gmail.com>
2015-12-25 19:18:27 -0600

    edit cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates special "matrix" object that can cache its inverse
+## cacheSolve:This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix. If the inverse has already been calculated (and the matrix  
+## has not changed), then the cacheSolve will retrieve the inverse from the cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv<<- NULL
+  }
+get <- function()x
+    setinverse <- function (inverse)inv <<- inverse
+    getinverse <- function()inv
+    list(set = set, get = get, 
+         setinverse = setinverse, 
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Following function "CacheSolve" will check whether 
+## the inverse is computed. If already calculated it will be 
+## retrieved from the cache; if not then it will be computed
+## and the value will be set in the cache through 
+## "setinverse" function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  # if the inverse has already been calculated
+  if (!is.null(inv)){
+  # gets it from cache and skips the computation
+    message("getting cached data")
+    return(inv)
+  }
+  # if not calculates the inverse
+  data <- x$get()
+  inv <- solve(data, ...)
+  # sets the value of inverse in the cache via the setinv function
+  x$setinv(inv)
+  inv
 }
+
+##Solution after running a sample data in R:
+
+## x <- rbind (c(2,4), c(4,2))
+## d = makeCacheMatrix(x)
+## d$get()
+## [,1] [,2]
+## [1,]    2    4
+## [2,]    4    2
+## cacheSolve(d)
+##  [,1]       [,2]
+## [1,] -0.1666667  0.3333333
+## [2,]  0.3333333 -0.1666667
+
+## Second run:
+## cacheSolve(d)
+## getting cached data
+##   [,1]       [,2]
+## [1,] -0.1666667  0.3333333
+## [2,]  0.3333333 -0.1666667
+  
